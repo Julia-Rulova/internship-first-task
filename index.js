a8289450d826741605805d2943d8fd7cdeb15596
Julia-Rulova <julia.rulova0066@gmail.com>
2023-04-26 11:10:13 +0300

    feat/open-close filter popup

--- index.js
+++ index.js
@@ -5,6 +5,11 @@ let btnSortTextMobile = document.querySelector(".search__btn-text_mobile");
 let popupSort = document.querySelector(".popup-sort");
 let popupSortMobile = document.querySelector(".popup-sort_mobile");
 let popupItems = document.querySelectorAll(".popup-sort__item");
+let popupFilters = document.querySelector(".popup-filters");
+let popupFiltersMobile = document.querySelector(".popup-filters_mobile");
+let filterBtn = document.querySelector(".search__btn_filters");
+let filterBtnMobile = document.querySelector(".search__btn_filters-mobile");
+let filtersSubmitBtns = document.querySelectorAll(".popup-filters__submit-btn");
 
 function putCheck() {
     for (let i = 0; i < popupItems.length; i++) {
@@ -42,3 +47,20 @@ popupSortMobile.addEventListener("click", (evt) => {
     btnSortTextMobile.textContent = evt.target.textContent;
     btnSortText.textContent = btnSortTextMobile.textContent;
 });
+
+filterBtn.addEventListener("click", () => {
+    popupFilters.classList.toggle("popup-filters_open");
+});
+
+filterBtnMobile.addEventListener("click", () => {
+    popupFiltersMobile.classList.toggle("popup-filters_open");
+});
+
+filtersSubmitBtns.forEach((btn) => {
+    btn.addEventListener("click", (evt) => {
+        evt.preventDefault();
+        popupFilters.classList.remove("popup-filters_open");
+        popupFiltersMobile.classList.remove("popup-filters_open");
+        console.log("Данные отфильтрованы!")
+    })
+})
